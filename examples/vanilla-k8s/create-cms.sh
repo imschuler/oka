12319de6c8023691f6031f5b870f243b0ab71b8b
root <root@oka.localdomain>
2025-11-30 09:32:12 +0100

    fixed some typos

--- examples/vanilla-k8s/create-cms.sh
+++ examples/vanilla-k8s/create-cms.sh
@@ -7,4 +7,4 @@ kubectl create configmap ansible-vars -n $NAMESPACE --from-file=vars.yaml
 
 (cd tasks; kubectl create configmap ansible-tasks -n $NAMESPACE --from-file=01-containerd.yaml  --from-file=02-cni.yaml  --from-file=03-kubelet.yaml --from-file=04-other.yaml)
 
-(cd templates; kubectl create configmap ansible-templates -n $NAMESPACE --from-file=proxy.j2 --from-file=chrony.j2)
+(cd templates; kubectl create configmap ansible-templates -n $NAMESPACE --from-file=proxy.j2 --from-file=chrony.j2 --from-file=kubeadm-init.j2 --from-file=kubeadm-join.j2)
